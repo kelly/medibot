4c4e516faf8d5a6b658225054f9098cb1ec02cfe
kkorevec <korevec@gmail.com>
2012-11-18 14:29:53 -0800

    changed ip back to medibot. Need to make this dynamic

--- public/javascripts/medibot-client.js
+++ public/javascripts/medibot-client.js
@@ -473,7 +473,7 @@
 
     Hud.prototype.initialize = function() {
       var _this = this;
-      Medibot.socket = io.connect('http://192.168.0.196');
+      Medibot.socket = io.connect('http://192.168.0.192');
       this.battery = new Medibot.Models.Sensor({
         min: 410,
         max: 565
